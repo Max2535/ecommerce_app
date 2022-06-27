96b520b975a0169ebf92c2399ee50dd77f9f3f16
max <kalmro4@gmail.com>
2022-06-27 20:48:33 +0700

    Working with Firebase

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,15 +1,28 @@
+import 'package:ecommerce_app/const.dart';
+
 class AuthController {
   //function to sign up users
-  singUpUser(
+  Future<String> singUpUser(
       String full_name, String username, String email, String password) async {
-    String res = '';
+    String res = 'error';
     try {
       if (full_name.isNotEmpty &&
           username.isNotEmpty &&
           email.isNotEmpty &&
-          password.isNotEmpty) {}
+          password.isNotEmpty) {
+        var cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        print(cred.user!.email);
+        res = 'success';
+      } else {
+        res = 'Please fields must not be empty';
+      }
     } catch (error) {
       print(error);
+      res = error.toString();
     }
+    return res;
   }
 }
